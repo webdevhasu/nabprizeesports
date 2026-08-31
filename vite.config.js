08a7e969247bfbcf46531be07050f5ef3d8da3ba
webdevhasu <webdevhasu@users.noreply.github.com>
2026-08-31 13:18:31 +0500

    Fix favicon to use custom icon, remove Vite default SVG

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icon-192.png', 'icon-512.png'],
+      includeAssets: ['icon-192.png', 'icon-512.png'],
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
